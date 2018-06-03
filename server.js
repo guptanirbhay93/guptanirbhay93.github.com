eddcbaf863118772c46af5d61c0a84ea33593611
guptanirbhay93 <guptanirbhay93@gmail.com>
2018-06-03 06:03:38 +0530

    test

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer();
+
+server.on('request', function (req, res) {
+  console.log(req.url)
+  const path = `./${req.url === '/' ? 'index.html' : req.url}`
+  fs.readFile(path, (err, data) => {
+    if (!err) {
+      res.end(data);
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
+
+  });
+});
+
+server.listen(8000);
